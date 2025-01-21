877328f689e3484d4e377cc89b0d6bac9507544f
Victor Andrade <victor64gaming@gmail.com>
2025-01-21 15:07:42 -0300

    Initial setup

---
+++ src/test/java/com/victoandrad/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victoandrad.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

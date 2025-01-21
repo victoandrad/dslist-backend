2dc0877e5d415d97becb1796d29d40c5e0ee9ace
Victor Andrade <victor64gaming@gmail.com>
2025-01-21 20:41:10 -0300

    GameMinDTO

---
+++ src/main/java/com/victoandrad/dslist/dtos/GameMinDTO.java
@@ -0,0 +1,75 @@
+package com.victoandrad.dslist.dtos;
+
+import com.victoandrad.dslist.models.Game;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Long id,
+                      String title,
+                      Integer year,
+                      String imgUrl,
+                      String shortDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+    }
+
+    public GameMinDTO(Game entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.year = entity.getYear();
+        this.imgUrl = entity.getImgUrl();
+        this.shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+}
